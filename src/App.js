4ff4d9556f94f0db8b102f4a351af33d01c31f63
LeDucThang <leducthang98@gmail.com>
2021-01-01 18:13:40 +0700

    add logger

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import routers from './component/router';
 import CommonConfig from './config/CommonConfig';
 import { corsMiddleware } from './middleware/Cors';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 const expressApp = express();
+
+var loggerStream = fs.createWriteStream(path.join(__dirname, '../system.log'), { flags: 'a' })
+expressApp.use(morgan('combined', { stream: loggerStream }))
+expressApp.use(morgan('dev'))
 
 expressApp.use(corsMiddleware);
 expressApp.use(bodyParser.json({
@@ -19,9 +26,9 @@ expressApp.use(bodyParser.urlencoded({
 for (const router of routers) {
   expressApp.use(router.path, router.router)
 }
+expressApp.use('/media', express.static('media'))
 
 expressApp.use(errorHandler)
-
 expressApp.listen(CommonConfig.PORT, () => {
   console.log('Server is running at port', CommonConfig.PORT)
 });
