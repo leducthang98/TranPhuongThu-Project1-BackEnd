6fe5eb7cf828d4dd009c3fc9ff4cfdeae28ecc16
LeDucThang <leducthang98@gmail.com>
2021-01-09 00:13:28 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -7,8 +7,28 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import fs from 'fs';
 import path from 'path';
+import multer from 'multer';
+import { commonResponse } from './util/ResponseForm';
+import { jwtFilter } from './middleware/Authenticate';
 
 const expressApp = express();
+
+// multer 
+const maxSizeImageUpload = 2 * 1024 * 1024;
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'media')
+  },
+  filename: function (req, file, cb) {
+    cb(null, (Date.now() + '-' + file.originalname))
+  }
+})
+
+var upload = multer({
+  storage: storage,
+  limits: { fileSize: maxSizeImageUpload },
+})
+
 
 var loggerStream = fs.createWriteStream(path.join(__dirname, '../system.log'), { flags: 'a' })
 expressApp.use(morgan('combined', { stream: loggerStream }))
@@ -26,6 +46,19 @@ expressApp.use(bodyParser.urlencoded({
 for (const router of routers) {
   expressApp.use(router.path, router.router)
 }
+
+expressApp.post('/upload/photo', jwtFilter, upload.single('image'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  res.send(commonResponse({
+    imagePath: file.path
+  }))
+})
+
 expressApp.use('/media', express.static('media'))
 
 expressApp.use(errorHandler)
